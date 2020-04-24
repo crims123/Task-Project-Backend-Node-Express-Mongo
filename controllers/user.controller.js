b8b4a6337dfc51f43643dad1c096fce060a5a7ea
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-04-24 12:07:14 -0500

    Added addUser validations

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,44 +1,75 @@
 const jwt = require('jsonwebtoken');
-const usuarioCtrl = {};
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const userCtrl = {};
 const jwtFirma = 'mypass';
 const User = require('../models/User');
 
-usuarioCtrl.addUser = async (req, res) => {
-    console.log(req.body)
-    const { Nombre, Apellido, Email, Contrasena } = req.body;
-    const usuario = new User({
-        Nombre,
-        Apellido,
-        Email,
-        Contrasena
-    });
-    await usuario.save();
-    res.json({
-        sucess: 'true',
-        message: 'Usuario creado'
-    });
-}
+userCtrl.addUser = async (req, res) => {
+  const errors = validationResult(req);
 
-usuarioCtrl.login = async (req, res) => {
-    const { Email, Contrasena } = req.body;
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const filtro = { Email, Contrasena };
-    let usuario = await Usuario.findOne(filtro);
+  try {
+    const { name, email, password, register } = req.body;
 
-    if (usuario) {
-        usuario.Contrasena = '';
-        const token = jwt.sign(usuario.toObject(), jwtFirma);
-        res.json({
-            sucess: 'true',
-            message: 'Usuario logueado',
-            token
-        });
-    } else {
-        res.status(400).json({
-            sucess: 'false',
-            message: 'Usuario no encontrado'
-        });
+    const uniqueEmail = await User.findOne({ email });
+
+    if (uniqueEmail) {
+      return res.status(400).json({
+        success: false,
+        message: 'Email has already been registered',
+      });
     }
-}
 
-module.exports = usuarioCtrl;
+    const user = new User({
+      name,
+      email,
+      password,
+      register,
+    });
+
+    // Hash Password
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(password, salt);
+
+    await user.save();
+
+    res.json({
+      success: true,
+      message: 'User created',
+      data: { user },
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error,
+    });
+  }
+};
+
+userCtrl.login = async (req, res) => {
+  const { Email, Contrasena } = req.body;
+
+  const filtro = { Email, Contrasena };
+  let usuario = await Usuario.findOne(filtro);
+
+  if (usuario) {
+    usuario.Contrasena = '';
+    const token = jwt.sign(usuario.toObject(), jwtFirma);
+    res.json({
+      sucess: 'true',
+      message: 'Usuario logueado',
+      token,
+    });
+  } else {
+    res.status(400).json({
+      sucess: 'false',
+      message: 'Usuario no encontrado',
+    });
+  }
+};
+
+module.exports = userCtrl;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 const { addUser, login } = require('../controllers/user.controller');
+const { check } = require('express-validator');
 
-router.route('/')
-    .post(addUser)
+router
+  .route('/', [
+    check('name', 'The name is mandatory').not().isEmpty(),
+    check('email', 'Add a valid email').isEmail(),
+    check(
+      'password',
+      'The password must be a minimum of 6 characters'
+    ).isLength({ min: 6 }),
+  ])
+  .post(addUser);
 
-router.route('/login')
-    .post(login);
+router.route('/login').post(login);
 
 module.exports = router;
