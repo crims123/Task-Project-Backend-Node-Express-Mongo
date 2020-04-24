71d4000550391d518c562e327b1335dc4750121f
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-04-24 14:58:04 -0500

    Added express-validator

--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,30 @@ const { addUser, login } = require('../controllers/user.controller');
 const { check } = require('express-validator');
 
 router
-  .route('/', [
-    check('name', 'The name is mandatory').not().isEmpty(),
-    check('email', 'Add a valid email').isEmail(),
-    check(
-      'password',
-      'The password must be a minimum of 6 characters'
-    ).isLength({ min: 6 }),
-  ])
-  .post(addUser);
+  .route('/')
+  .post(
+    [
+      check('name', 'The name is mandatory').not().isEmpty(),
+      check('email', 'Add a valid email').isEmail(),
+      check(
+        'password',
+        'The password must be a minimum of 6 characters'
+      ).isLength({ min: 6 }),
+    ],
+    addUser
+  );
 
-router.route('/login').post(login);
+router
+  .route('/login')
+  .post(
+    [
+      check('email', 'Add a valid email').isEmail(),
+      check(
+        'password',
+        'The password must be a minimum of 6 characters'
+      ).isLength({ min: 6 }),
+    ],
+    login
+  );
 
 module.exports = router;
