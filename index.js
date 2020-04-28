ac4658635559275bac4960626707115f89d876d6
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-04-28 13:57:05 -0500

    Added cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('./database');
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.set('port', 4000);
 // Middelwares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
