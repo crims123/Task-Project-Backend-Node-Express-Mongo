02ec8585e76fceba5afa2cea4bae0bc6cc83b0fb
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-05 06:36:04 -0500

    Refactor index.js

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 const port = process.env.PORT || 4000;
 
-// Middelwares
+// Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
@@ -16,10 +15,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 
-app.get('/', (_, res) => {
-  res.json('Conected');
-});
-
-app.listen(process.env.PORT || 5000, function () {
+app.listen(port, '0.0.0.0', () => {
   console.log('Server running');
 });
